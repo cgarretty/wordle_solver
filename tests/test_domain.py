d8334efaed053d90a2743947f6012f00b53f6569
cgarretty <35119415+cgarretty@users.noreply.github.com>
2023-08-29 21:31:29 -0400

    add another test.

--- tests/test_domain.py
+++ tests/test_domain.py
@@ -1,22 +1,18 @@
 import pytest
 from domain import wordle
 
+
 def test_guess_result_is_correct():
-    game = wordle.Board(answer='peace')
-    guess = 'serai'
-    test_result = [
-        wordle.GRAY,
-        wordle.GREEN, 
-        wordle.GRAY, 
-        wordle.YELLOW, 
-        wordle.GRAY
-    ]
-    
+    game = wordle.Board(answer="peace")
+    guess = "serai"
+    test_result = [wordle.GRAY, wordle.GREEN, wordle.GRAY, wordle.YELLOW, wordle.GRAY]
+
     assert test_result == game.score(guess)
+
 
 def test_tougher_example_is_correct():
-    game = wordle.Board(answer='stops')
-    guess = 'books'
+    game = wordle.Board(answer="stops")
+    guess = "books"
 
     test_result = [
         wordle.GRAY,
@@ -27,3 +23,15 @@ def test_tougher_example_is_correct():
     ]
 
     assert test_result == game.score(guess)
+
+
+def test_with_repeated_letters_in_answer():
+    game = wordle.Board(answer="steep")
+    guess = "elect"
+    test_results = [
+        wordle.YELLOW,
+        wordle.GRAY,
+        wordle.GREEN,
+        wordle.GRAY,
+        wordle.GRAY,
+    ]
